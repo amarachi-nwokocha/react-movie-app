fb1e1ff8d43cc9732a73d77fb992ad325459f003
amarachi-nwokocha <63500921+amarachi-nwokocha@users.noreply.github.com>
2023-02-12 23:09:28 -0800

    learning how react query works

---
+++ src/components/People.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const people = () => {
+  return (
+    <h1>people</h1>
+  )
+}
+
+export default people
---
+++ src/components/Planets.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const planets = () => {
+  return (
+    <h1>planets</h1>
+  )
+}
+
+export default planets
